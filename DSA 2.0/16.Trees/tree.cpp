ca5282358e3aa87ae01781b326f07424a3b25cc4
priyanshu <priyanshudalal75@gmail.com>
2023-08-12 16:59:34 +0530

    Traversal Height and Depth of a tree

--- DSA 2.0/16.Trees/tree.cpp
+++ DSA 2.0/16.Trees/tree.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class Node{
@@ -28,8 +29,70 @@ Node* buildTree(){
     root->right=buildTree();
     return root;
 }
+void levelOrderTraversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node* temp=q.front();
+        q.pop();
+        if(temp==NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+void inOrderTraversal(Node* root){
+    if(root==NULL){
+        return;
+    }
+    inOrderTraversal(root->left);
+    cout<<root->data<<" ";
+    inOrderTraversal(root->right);
+}
+void preOrderTraversal(Node* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+void postOrderTraversal(Node* root){
+    if(root==NULL){
+        return;
+    }
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout<<root->data<<" ";
+}
+int height(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int leftHeight=height(root->left);
+    int rightHeight=height(root->right);
+    int ans=max(leftHeight, rightHeight)+1;
+    return ans;
+}
 int main(){
     Node* root=NULL;
     root=buildTree();
+    // preOrderTraversal(root);
+    // inOrderTraversal(root);
+    // levelOrderTraversal(root);
+    // postOrderTraversal(root);
+    cout<<"Height of a tree is : "<<height(root)<<endl;
     return 0;
 }
